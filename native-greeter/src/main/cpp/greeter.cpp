fd44a8039788972b05488279da0382ac547be3ed
Daniel Lacasse <daniel@gradle.com>
2019-11-27 14:52:53 +0400

    Return string instead

--- native-greeter/src/main/cpp/greeter.cpp
+++ native-greeter/src/main/cpp/greeter.cpp
@@ -1,8 +1,7 @@
 #include "greeter.h"
 
-#include <iostream>
 #include <string>
 
-void say_hello(std::string name) {
-    std::cout << "Bonjour, " << name << "!" << std::endl;
+std::string say_hello(std::string name) {
+    return "Bonjour, " + name + "!";
 }
--- native-greeter/src/main/public/greeter.h
+++ native-greeter/src/main/public/greeter.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include <iostream>
 #include <string>
 
-void say_hello(std::string name);
+std::string say_hello(std::string name);
