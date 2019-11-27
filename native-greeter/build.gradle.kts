bc6d4a9d1558c589544625c97671a938f042100f
Paul Merlin <paul@gradle.com>
2019-11-27 14:37:19 +0100

    set std=c++11 in :native-greeter
    Signed-off-by: Paul Merlin <paul@gradle.com>

--- native-greeter/build.gradle.kts
+++ native-greeter/build.gradle.kts
@@ -4,4 +4,10 @@ plugins {
 
 library {
     linkage.set(listOf(Linkage.STATIC))
+    binaries.configureEach {
+        compileTask.get().compilerArgs.addAll(compileTask.get().toolChain.map {
+            if (it is Gcc || it is Clang) listOf("--std=c++11")
+            else emptyList()
+        })
+    }
 }
